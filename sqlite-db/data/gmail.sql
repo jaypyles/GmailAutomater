ce520b8dc1ebbca5b595a6b4b796d47bae238575
Jayden Pyles <jpylesbusiness@gmail.com>
2023-08-16 20:32:30 -0500

    feat: add sqlite database for interaction with emails

--- sqlite-db/data/gmail.sql
+++ sqlite-db/data/gmail.sql
@@ -1,11 +1,30 @@
-CREATE TABLE IF NOT EXISTS emails (
+CREATE TABLE
+  IF NOT EXISTS keep_emails (
     id INTEGER PRIMARY KEY,
     sender TEXT NOT NULL,
     subject TEXT,
     body TEXT
-);
+  );
 
-INSERT INTO emails (sender, subject, body)
+CREATE TABLE
+  IF NOT EXISTS delete_emails (
+    id INTEGER PRIMARY KEY,
+    sender TEXT NOT NULL UNIQUE
+  );
+
+CREATE TABLE
+  IF NOT EXISTS labels (
+    id INTEGER PRIMARAY KEY,
+    name TEXT NOT NULL UNIQUE
+  );
+
+INSERT INTO
+  delete_emails (sender)
 VALUES
-    ('john@example.com', 'Hello', 'This is a test email.'),
-    ('jane@example.com', 'Important Notice', 'Please read the attached document.');
+  ('vfe-campaign-response@amazon.com'),
+  ('jane@example.com');
+
+INSERT INTO
+  labels (name)
+VALUES
+  ('Transactions');
